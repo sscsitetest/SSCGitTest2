cb20ff6ef311cbe8f12d87d7d34f9261c35ed6ba
ItactDev <itactdev1@itactsolutions.com>
2018-08-30 17:25:34 +0530

    tet

---
+++ GithubTest1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GithubTest1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
